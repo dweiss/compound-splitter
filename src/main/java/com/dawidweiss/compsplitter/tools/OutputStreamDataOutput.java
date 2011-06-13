38dd35c08a8417267207aa1e9694584b90208b2f
Dawid Weiss <dawid.weiss@carrot-search.com>
2011-06-13 22:44:48 +0200

    Cleanups. FST only from surface forms (no POSs).

---
+++ src/main/java/com/dawidweiss/compsplitter/tools/OutputStreamDataOutput.java
@@ -0,0 +1,33 @@
+package com.dawidweiss.compsplitter.tools;
+
+import java.io.*;
+
+import org.apache.lucene.store.DataOutput;
+
+/**
+ * A {@link DataOutput} wrapping a plain {@link OutputStream}.
+ */
+public class OutputStreamDataOutput extends DataOutput implements Closeable {
+  
+  private final OutputStream os;
+  
+  public OutputStreamDataOutput(OutputStream os) {
+    this.os = os;
+  }
+  
+  @Override
+  public void writeByte(byte b) throws IOException {
+    os.write(b);
+  }
+  
+  @Override
+  public void writeBytes(byte[] b, int offset, int length) throws IOException {
+    os.write(b, offset, length);
+  }
+  
+  @Override
+  public void close() throws IOException
+  {
+      this.os.close();
+  }
+}
